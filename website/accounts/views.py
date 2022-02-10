2b5a6def6956e42d21f9d93ce7ce2d6361dfc855
cgaona1 <gaonachris930@gmail.com>
2022-02-10 14:50:54 -0700

    Added a view, model, and form user notification

--- website/accounts/views.py
+++ website/accounts/views.py
@@ -1,7 +1,8 @@
 from re import template
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.forms import UserCreationForm
+from .forms import NotificationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -13,5 +14,6 @@ class SignUpView(generic.CreateView):
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
-class NotificationPageView(TemplateView):
+class NotificationPageView(CreateView):
+    form_class = NotificationForm
     template_name = "email_notification_page.html"
