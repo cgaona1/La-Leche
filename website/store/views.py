215d70a66aa89225ca0d6f2cfd7cd20cdbf4954b
Caden-Bonnell <70333456+Caden-Bonnell@users.noreply.github.com>
2022-03-10 09:59:23 -0700

    delete view and redirect link

--- website/store/views.py
+++ website/store/views.py
@@ -1,7 +1,7 @@
 
 
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView, ListView, UpdateView
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView
 from .models import Product
 from django.shortcuts import render
 # Create your views here.
@@ -40,10 +40,10 @@ class CreateItem(CreateView):
     model = Product
     template_name = "add_item.html"
     fields = ['name', 'cost', 'quantity', 'image']
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('owner')
 
 
-class DeletePage(TemplateView):
+class DeletePage(DeleteView):
     model = Product
     template_name = "delete_item.html"
     success_url = reverse_lazy('owner')
